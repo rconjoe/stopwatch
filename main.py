98314320b78f8902c3ab232194b980f60f3e7cf0
trog <admin@trog.host>
2024-04-16 16:37:49 -0700

    custom component

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from textual.widgets import (
     Header,
     Footer,
     Button,
+    Static,
 )
+
+
+class TimeDisplay(Static):
+    pass
+
+
+class Stopwatch(Static):
+    def compose(self):
+        yield Button("Start", variant="success")
+        yield Button("Stop", variant="error")
+        yield Button("Reset")
+        yield TimeDisplay("00:00:00.00")
 
 
 class StopwatchApp(App):
@@ -13,6 +26,7 @@ class StopwatchApp(App):
 
     def compose(self):
         yield Header(show_clock=True)
+        yield Stopwatch()
         yield Footer()
 
     def action_toggle_dark_mode(self):
