bf0eaec6e186ee9ee763685215c77818ef6fb660
trog <admin@trog.host>
2024-04-16 16:32:10 -0700

    bind action to key

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ from textual.widgets import (
 
 
 class StopwatchApp(App):
+    BINDINGS = [
+        ("d", "toggle_dark_mode", "Toggle dark mode"),
+    ]
+
     def compose(self):
         yield Header(show_clock=True)
-        yield Button("Start")
-        yield Button("Stop")
         yield Footer()
+
+    def action_toggle_dark_mode(self):
+        self.dark = not self.dark
 
 
 if __name__ == "__main__":
