93a6b2aeac159f2c73816bcef766bcc3f3897742
trog <admin@trog.host>
2024-04-16 18:30:04 -0700

    cumulative stopwatch logic

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ from textual.widgets import (
 
 
 class TimeDisplay(Static):
+    accumulated_time = 0
+    start_time = monotonic()
     time_elapsed = reactive(0)
+
+    def on_mount(self):
+        """this is basically useeffect, with built in scheduling"""
+        self.update_timer = self.set_interval(
+            1 / 60,
+            self.update_time_elapsed,
+            pause=True,
+        )
+
+    def update_time_elapsed(self):
+        self.time_elapsed = self.accumulated_time + (monotonic() - self.start_time)
 
     def watch_time_elapsed(self):
         time = self.time_elapsed
@@ -24,12 +37,15 @@ class TimeDisplay(Static):
 
     def start(self):
         self.start_time = monotonic()
+        self.update_timer.resume()
 
     def stop(self):
-        self.time_elapsed = monotonic() - self.start_time
+        self.accumulated_time = self.time_elapsed
+        self.update_timer.pause()
 
     def reset(self):
-        pass
+        self.accumulated_time = 0
+        self.time_elapsed = 0
 
 
 class Stopwatch(Static):
