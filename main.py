f81420fae8e8155ef40ff5c74b97152fb5c2ec4e
trog <admin@trog.host>
2024-04-16 18:11:02 -0700

    implement reactive attributes and time tracking

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from time import monotonic
+
 from textual import on
 from textual.app import App
 from textual.containers import ScrollableContainer
+from textual.reactive import reactive
 from textual.widgets import (
     Header,
     Footer,
@@ -10,17 +13,39 @@ from textual.widgets import (
 
 
 class TimeDisplay(Static):
-    pass
+    time_elapsed = reactive(0)
+
+    def watch_time_elapsed(self):
+        time = self.time_elapsed
+        time, seconds = divmod(time, 60)
+        hours, minutes = divmod(time, 60)
+        time_string = f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}"
+        self.update(time_string)
+
+    def start(self):
+        self.start_time = monotonic()
+
+    def stop(self):
+        self.time_elapsed = monotonic() - self.start_time
+
+    def reset(self):
+        pass
 
 
 class Stopwatch(Static):
     @on(Button.Pressed, "#start")
     def start_stopwatch(self):
         self.add_class("started")
+        self.query_one(TimeDisplay).start()
 
     @on(Button.Pressed, "#stop")
     def stop_stopwatch(self):
         self.remove_class("started")
+        self.query_one(TimeDisplay).stop()
+
+    @on(Button.Pressed, "#reset")
+    def reset_stopwatch(self):
+        self.query_one(TimeDisplay).reset()
 
     def compose(self):
         yield Button("Start", variant="success", id="start")
